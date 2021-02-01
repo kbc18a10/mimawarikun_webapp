d152f5f5c57f393f6b9435f4a729261e9ffeaf88
kbc18a10 <kbc18a10@stu.kawahara.ac.jp>
2021-02-01 10:02:49 +0900

    ページネーション

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { render } from 'react-dom'
 import Header from './components/Header'
 import Router from './components/Router'
 
-import './styles/roomList.css'
 
 function App() {
   return (
--- src/components/RoomList/index.js
+++ src/components/RoomList/index.js
@@ -1,30 +1,80 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
 import '../../styles/roomList.css'
 
 
 export default class RoomList extends Component{
+    constructor(props){
+        super(props)
+        //const rooms = [{name:101},{name:102},{name:103},{name:104},{name:105},{name:106},{name:107}] //仮
+        const rooms = []
+        this.state = {
+            token:'7BOtayK7Y7a7urkG03MjLrKCejeoKATfTYOiyXXK9zxNsIMDTZcRAfYCWZQt',
+            roomInfo:rooms,
+            index:1,
+            page:Math.ceil(rooms.length / 5),
+        }
+        this.setRoomInfo()
+        this.setIndex = this.setIndex.bind(this);
+    }
+
+    async getRoomInfo(url) {
+        const headers = {'Content-Type': 'application/json','token':this.state.token}
+        try{
+            const response = await (await fetch(url,{mode: 'no-cors', headers: headers})).text() 
+            console.log(response)
+        }catch(error){
+            console.error('Error:', error)
+        }
+    }
+
+    setRoomInfo(){
+        this.getRoomInfo('http://localhost:8085/api/room')
+    }
+
+    setIndex(e){
+        this.setState({index:e.target.value})
+    }
+
+    getPagenation(){
+        var buttons = []
+        for(let i = 1;i <= this.state.page;i++){
+            var str = <button type="button" id={"page"+i} value={i} onClick={this.setIndex}>{i}</button>
+            buttons.push(str)
+        }
+        var element = React.createElement('div',{id:'page'},buttons)
+        return element
+    }
+
+    getTable(){
+        var str = '<table id="resultTable" border="1"><tbody>'
+        const startIndex = (this.state.index - 1) * 5
+        let endIndex = startIndex + 4
+        if(this.state.roomInfo.length < endIndex){
+            endIndex = this.state.roomInfo.length - 1
+        }
+        this.state.roomInfo.forEach((element,index) => {
+            if(startIndex <= index && index <= endIndex){
+                str += `<tr><th>${element['name']}</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>`
+            }
+        })
+        str += '</tbody></table>'
+        return str
+    }
     
     render(){
+        const roomTable = this.getTable()
+        const pagenation = this.getPagenation()
         return (
             <div>
                 <img id="menu" src="https://s3-ap-northeast-1.amazonaws.com/mimawarikun.strage/tai.png" />
                 <div className="roomlist">
                     <table id="resultTable" border="1">
-                        <tbody><tr><th>101</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
-                        <tr><th>102</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
-                        <tr><th>103</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
-                        <tr><th>201</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
-                        <tr><th>202</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
-                        <tr><th>203</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
-                        <tr><th>204</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr></tbody>
+                        <tbody dangerouslySetInnerHTML={{ __html: roomTable }}></tbody>
                     </table>
-                </div>
-                <div className="pagenation">
-                    <button type="button" id="page1">1</button>
-                    <button type="button" id="page2">2</button>
-                    <button type="button" id="page3">3</button>
+                </div>   
+                <div className="pagenation" id="pagenation">
+                    {pagenation}
                 </div>
                 <div className="btn_area">
                     <Link to={`/addroom`}>
