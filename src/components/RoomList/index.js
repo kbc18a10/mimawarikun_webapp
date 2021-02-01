e75c6bcb0eb17b1a679b0e8f80d7f1123049ff97
kbc18a10 <kbc18a10@stu.kawahara.ac.jp>
2021-02-01 10:50:28 +0900

    fix RoomList

--- src/components/RoomList/index.js
+++ src/components/RoomList/index.js
@@ -6,15 +6,15 @@ import '../../styles/roomList.css'
 export default class RoomList extends Component{
     constructor(props){
         super(props)
-        //const rooms = [{name:101},{name:102},{name:103},{name:104},{name:105},{name:106},{name:107}] //仮
-        const rooms = []
+        const rooms = [{name:101},{name:102},{name:103},{name:104},{name:105},{name:106},{name:107}] //仮
+        //const rooms = []
         this.state = {
             token:'7BOtayK7Y7a7urkG03MjLrKCejeoKATfTYOiyXXK9zxNsIMDTZcRAfYCWZQt',
             roomInfo:rooms,
             index:1,
             page:Math.ceil(rooms.length / 5),
         }
-        this.setRoomInfo()
+        //this.setRoomInfo()
         this.setIndex = this.setIndex.bind(this);
     }
 
@@ -39,27 +39,28 @@ export default class RoomList extends Component{
     getPagenation(){
         var buttons = []
         for(let i = 1;i <= this.state.page;i++){
-            var str = <button type="button" id={"page"+i} value={i} onClick={this.setIndex}>{i}</button>
-            buttons.push(str)
+            var btn = <button type="button" id={"page"+i} value={i} onClick={this.setIndex}>{i}</button>
+            buttons.push(btn)
         }
         var element = React.createElement('div',{id:'page'},buttons)
         return element
     }
 
     getTable(){
-        var str = '<table id="resultTable" border="1"><tbody>'
         const startIndex = (this.state.index - 1) * 5
         let endIndex = startIndex + 4
         if(this.state.roomInfo.length < endIndex){
             endIndex = this.state.roomInfo.length - 1
         }
+        var rows = []
         this.state.roomInfo.forEach((element,index) => {
             if(startIndex <= index && index <= endIndex){
-                str += `<tr><th>${element['name']}</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>`
+                var row = <tr><th>{element['name']}</th><td><a href="./edit">編集</a></td><td><a href="./delete">削除</a></td></tr>
+                rows.push(row)
             }
         })
-        str += '</tbody></table>'
-        return str
+        var element = React.createElement('table',{id:'resultTable',border:'1'},rows)
+        return element
     }
     
     render(){
@@ -70,7 +71,7 @@ export default class RoomList extends Component{
                 <img id="menu" src="https://s3-ap-northeast-1.amazonaws.com/mimawarikun.strage/tai.png" />
                 <div className="roomlist">
                     <table id="resultTable" border="1">
-                        <tbody dangerouslySetInnerHTML={{ __html: roomTable }}></tbody>
+                        {roomTable}
                     </table>
                 </div>   
                 <div className="pagenation" id="pagenation">
