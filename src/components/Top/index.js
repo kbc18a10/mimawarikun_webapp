27020c31b3882f47da2319abb7361c7a17a380ca
wadanaoumi <wada121400@gmail.com>
2021-02-10 10:37:20 +0900

    見出し「表示タイプを選択」を追加

--- src/components/Top/index.js
+++ src/components/Top/index.js
@@ -3,26 +3,33 @@ import { Link } from 'react-router-dom'
 import '../../styles/Top.css'
 
 export default class Top extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            token: '',
+            roomList: [],
+            displayType: ''
+        };
+    }
+
     render() {
         return (
             <div>
-
                 <div id="radiobutton">
+                    <p className="radios-description">表示タイプを選択</p>
                     <p>
-                        <input type="radio"  name="r1" value="ichirannhyouji" class="radiobutton"/>一覧表示
-                        <input type="radio" name="r1" value="dennki" class="radiobutton"/>電気
-                        <input type="radio" name="r1" value="hitonoumu" class="radiobutton"/>人の有無
+                        <input type="radio" name="r1" value="ichirannhyouji" class="radiobutton" />一覧表示
+                        <input type="radio" name="r1" value="dennki" class="radiobutton" />電気
+                        <input type="radio" name="r1" value="hitonoumu" class="radiobutton" />人の有無
                         <input type="radio" name="r1" value="madonosejyou" class="radiobutton" />窓の施錠
-                        </p>
-
+                    </p>
                 </div>
-
                 <div>
                     <table id="maintable" border="1">
                         <tr>
                             <th>部屋番号</th><th>電気</th><th>人の有無</th><th>窓の施錠</th>
                         </tr>
-
                         <tr>
                             <th>100</th><th></th><th></th><th></th>
                         </tr>
@@ -36,24 +43,15 @@ export default class Top extends Component {
                         </tr>
                     </table>
                 </div>
-
                 <div id="button-area">
                     <button id="buttonn" type="button" name="suuji" value="1" >1</button>
                     <button id="buttonn" type="button" name="suuji" value="2" >2</button>
                     <button id="buttonn" type="button" name="suuji" value="3" >3</button>
                 </div>
-
                 <div id="button-area2">
                     <Link to={`/top`}><button id="button" type="button">部屋の追加</button></Link>
                     <Link to={`/RoomList`}><button id="button" type="button">部屋の一覧表示</button></Link>
                 </div>
-
-
-
-
-
-
-
             </div>
         )
     }
