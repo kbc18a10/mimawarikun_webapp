1c6a2357bfd12b1efdddc1695d2cb7aa52214aea
kbc18a10 <kbc18a10@stu.kawahara.ac.jp>
2021-02-15 11:35:32 +0900

    api連携

--- src/components/Syousai/index.js
+++ src/components/Syousai/index.js
@@ -1,57 +1,194 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { isNumber } from 'util'
-
+import { withRouter } from 'react-router';
+import axios from 'axios'
+import  tai2 from './images/tai2.png'
 import '../../styles/syousaihennsyuustyle.css'
 
 
-export default class syousaihennsyuu extends Component{
+class syousaihennsyuu extends Component{
     constructor(props){
         super(props)
         this.state = {
-            tekitou:[]
+            token:'7BOtayK7Y7a7urkG03MjLrKCejeoKATfTYOiyXXK9zxNsIMDTZcRAfYCWZQt',
+            id:this.getId(),
+            roomNo:'',
+            className:'',
+            roomInfo:{},
+            tekitou:[],
+            cameraAry:[],
+            done:false
         }
-           this.hanntei1 = this.hanntei1.bind(this);
+        this.hanntei1 = this.hanntei1.bind(this);
+        this.handleOnChangeRoomNo = this.handleOnChangeRoomNo.bind(this);
+        this.handleOnChangeClassName = this.handleOnChangeClassName.bind(this);
+        this.handleOnChangeCameraName = this.handleOnChangeCameraName.bind(this);
+        this.handleOnChangeUsetype = this.handleOnChangeUsetype.bind(this);
+        this.submitData = this.submitData.bind(this);
+        this.setRoomInfo()
+    }
+
+    getId(){
+        try{
+            var id = this.props.location.state.id
+            return id
+        }catch{
+            this.props.history.push('/roomlist')
+            return
+        }
+    }
+
+    async setRoomInfo(){
+        if(this.state.id == ''){
+            this.hanntei1()
+            return
+        }
+        var url = 'http://localhost:8085/api/room/' + this.state.id
+        axios.defaults.headers.common = {token: this.state.token}
+        try{
+            var response = await (await axios.get(url)).data
+            this.setState({roomInfo:response,roomNo:response.name,className:response.class,done:true})
+            this.hanntei1()
+        }catch(error){
+            console.error('Error:', error)
+        }
     }
 
     hanntei1(){
-        var row1=<tr><td class="noborder" >&nbsp;</td><td class="noborder" >&nbsp;</td></tr>
-        var row2 =<tr><td class="noborder" align="right">カメラ名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td ><input id={"cameranum"+this.state.tekitou.length} name="cameranamae" type="text" /></td></tr>
-        var row3=<tr><td class="noborder" align="right">      使用用途&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input id={"youtonum"+this.state.tekitou.length}name="youto" type="text" /></td></tr>      
-        
-        var element = React.createElement('tbody',{id:'aaa'},[row1,row2,row3])
-        var el =React.createElement('table',{id:'Detailedediting',border:'1'},element)
-        var elements= this.state.tekitou
-        elements.push(el);
-        this.setState({tekitou:elements });
+        if(!this.state.done){
+            return
+        }
+        var cameras = this.state.roomInfo.cameras
+        if(this.state.tekitou.length == 0){
+            if(cameras.length == 0){
+                this.addElement()
+            }else{
+                for(var i = 0;i<cameras.length;i++){
+                    this.addElement()
+                }
+            }
+        }else{
+            this.addElement()
+        }
     }
+
+    addElement(){
+        var len = this.state.cameraAry.length
+        var cameraAry = this.state.cameraAry
+        var cameraInfo = {
+            id:"",
+            name:"",
+            usetype:"",
+        }
+        cameraAry.push(cameraInfo)
+        this.setState({cameraAry:cameraAry})
+        var cameras = this.state.roomInfo.cameras
+        var id;
+        var nameValue;
+        var utValue;
+        try{
+            id = cameras[len].id
+        }catch{
+            id = ''
+        }
+        try{
+            nameValue = cameras[len].name
+        }catch{
+            nameValue = ''
+        }
+        try{
+            utValue = cameras[len].usetype
+        }catch{
+            utValue = ''
+        }
+        var row1 = <tr><td class="noborder" >&nbsp;</td><td class="noborder" >&nbsp;</td></tr>
+        var row2 = <tr><td class="noborder" align="right">カメラ名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input id={"cameranum"+len} type="text" onChange={e => this.handleOnChangeCameraName(e)} defaultValue={nameValue}/></td></tr>
+        var row3 = <tr><td class="noborder" align="right">      使用用途&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input id={"youtonum"+len} type="text" onChange={e => this.handleOnChangeUsetype(e)} defaultValue={utValue}/></td></tr>
+        var element_tbody = React.createElement('tbody',{},[row1,row2,row3])
+        var element_table =React.createElement('table',{id:'Detailedediting',border:'1'},element_tbody)
+        var ary = this.state.tekitou
+        ary.push(element_table)
+        cameraAry = this.state.cameraAry
+        cameraAry[len].id = id
+        cameraAry[len].name = nameValue
+        cameraAry[len].usetype = utValue
+        this.setState({tekitou:ary,cameraAry:cameraAry});
+    }
+
     bunnkatukunn(){
         var output =React.createElement('div',{id:'soton'},this.state.tekitou)
         return output;
     }
+
+    handleOnChangeRoomNo(e){
+        if(!this.state.done){
+            return
+        }
+        this.setState({roomNo:e.target.value})
+    }
+
+    handleOnChangeClassName(e){
+        if(!this.state.done){
+            return
+        }
+        this.setState({className:e.target.value})
+    }
+
+    handleOnChangeCameraName(e){
+        var index = e.target.id.slice(-1)
+        var cameraAry = this.state.cameraAry
+        cameraAry[index].name = e.target.value
+        this.setState({cameraAry:cameraAry})
+    }
+
+    handleOnChangeUsetype(e){
+        var index = e.target.id.slice(-1)
+        var cameraAry = this.state.cameraAry
+        cameraAry[index].usetype = e.target.value
+        this.setState({cameraAry:cameraAry})
+    }
+
+    async submitData(){
+        const roomNo = this.state.roomNo
+        const className = this.state.className
+        const cameraInfo = this.state.cameraAry
+        const roomInfo = {name:roomNo,class:className,cameras:cameraInfo};
+        var url = 'http://localhost:8085/api/room/' + this.state.id;
+        console.log(url)
+        try{
+            axios.defaults.headers.common = {token: this.state.token};
+            await (await axios.put(url,roomInfo));
+        }catch(error){
+            console.log('Error:',error)
+        }
+
+    }
+
     render(){
         var kekka= this.bunnkatukunn();
+        const roomNo = this.state.roomNo
+        const className = this.state.className
+        const cameraInfo = this.state.cameraAry
+        const roomInfo = {name:roomNo,class:className,cameras:cameraInfo};
+        console.log(roomInfo)
         return (
             <div id="syousai">
-                <img id="menu" src="https://s3-ap-northeast-1.amazonaws.com/mimawarikun.strage/tai2.png" />
+                <img id="menu" src={tai2.png} />
                 <div className="Detailedediting">
                     <table id="Detailedediting" border="1">
                     <tbody>
-                        <tr><td class="noborder"  align="right">部屋番号</td><td><input name="room" type="text" /></td></tr>
-　                      <tr><td class="noborder" align="right" >利用しているクラス</td><td><input name="class" type="text" /></td></tr>
+                        <tr><td class="noborder"  align="right">部屋番号</td><td><input name="room" type="text" onChange={e => this.handleOnChangeRoomNo(e)} value={this.state.roomNo}/></td></tr>
+　                      <tr><td class="noborder" align="right" >利用しているクラス</td><td><input name="class" type="text" onChange={e => this.handleOnChangeClassName(e)} value={this.state.className}/></td></tr>
                         <tr><td class="noborder" >&nbsp;</td><td class="noborder" >&nbsp;</td></tr>
-                        <tr><td class="noborder" align="right">      カメラ名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td ><input id="cameranum" name="cameranamae" type="text" /></td></tr>
-                        <tr><td class="noborder" align="right">      使用用途&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input id="youtonum"name="youto" type="text" /></td></tr>      
-                        </tbody>
+                    </tbody>
                     </table>
+                    {kekka}
+                    
                 </div>
-                {kekka}
-                 <button name="camera" value="true" onClick={this.hanntei1}>カメラを追加</button>
-                
-
+                <button name="camera" value="true" onClick={this.hanntei1}>カメラを追加</button>
                 <div className="btn_area">
-                <Link to={``}>
-                        <button >変更を保存する</button>
+                <Link to={`/roomlist`}>
+                        <button onClick={this.submitData}>変更を保存する</button>
                     </Link>
                     <Link to={`/roomlist`}>
                         <button id="to_roomlist">部屋の一覧表示</button>
@@ -64,3 +201,5 @@ export default class syousaihennsyuu extends Component{
         )
     }
 }
+
+export default  withRouter(syousaihennsyuu)
